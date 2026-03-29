bfa1217e91d95fa8a719df1390014a567f22011e
Gabriel Gnoatto <gnoattogabriel100@gmail.com>
2026-03-29 17:51:19 -0300

    restful

--- src/main/java/com/example/Tarefa/controllers/TarefaController.java
+++ src/main/java/com/example/Tarefa/controllers/TarefaController.java
@@ -3,6 +3,7 @@ package com.example.Tarefa.controllers;
 import com.example.Tarefa.models.TarefaModel;
 import com.example.Tarefa.service.TarefaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,27 +17,31 @@ public class TarefaController {
     private TarefaService tarefaService;
 
     @PostMapping
-    public TarefaModel criarTarefa(TarefaModel novaTarefa){
-        return tarefaService.criarTarefa(novaTarefa);
+    public ResponseEntity<TarefaModel> criarTarefa(@RequestBody TarefaModel novaTarefa){
+        TarefaModel tarefa = tarefaService.criarTarefa(novaTarefa);
+        return ResponseEntity.status(201).body(tarefa);
     }
 
     @GetMapping
-    public List<TarefaModel> listarTodos(){
-        return tarefaService.listarTodos();
+    public ResponseEntity<List<TarefaModel>> listarTodos(){
+        return ResponseEntity.ok(tarefaService.listarTodos());
     }
 
     @DeleteMapping("/{id}")
-    public void deletarTarefa(Long id){
+    public ResponseEntity<?> deletarTarefa(@PathVariable Long id){
         tarefaService.deletarTarefa(id);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{id}")
-    public Optional<TarefaModel> buscarPorId(Long id){
-        return tarefaService.buscarPorId(id);
+    public ResponseEntity<TarefaModel> buscarPorId(@PathVariable Long id){
+        return tarefaService.buscarPorId(id).map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PutMapping("/{id}")
-    public TarefaModel atualizarTarefa(@PathVariable Long id, @RequestBody TarefaModel novaTarefa){
-        return tarefaService.atualizarTarefa(id, novaTarefa);
+    public ResponseEntity<TarefaModel> atualizarTarefa(@PathVariable Long id, @RequestBody TarefaModel novaTarefa){
+        TarefaModel tarefaAtt = tarefaService.atualizarTarefa(id, novaTarefa);
+        return ResponseEntity.ok(tarefaAtt);
     }
 }
